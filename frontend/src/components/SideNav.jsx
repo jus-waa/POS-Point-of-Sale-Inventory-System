72c00336001de0d276ba3637234b6af889e3d1f1
josh <joshlagrimas1808@gmail.com>
2025-03-20 21:54:00 +0800

    Finished sidenav

--- frontend/src/components/SideNav.jsx
+++ frontend/src/components/SideNav.jsx
@@ -3,6 +3,7 @@ export default function SideNav(){
         <nav className="shadow-xl border-t-2 h-screen fixed left-0 top-[-4] flex flex-col bg-white">
             <div className="flex flex-col items-center p-3">
                 <ul className="flex flex-col space-y-4">
+                    {/* Dashboard / Analytics */}
                     <div className="border-y-2 p-2">
                         <li>
                             <a href="">
@@ -10,7 +11,8 @@ export default function SideNav(){
                             </a>
                         </li>
                     </div>
-                    <div className="flex flex-col space-y-2 border-y-2 p-2">
+                    {/* Product Management & Inventory Tracking */}
+                    <div className="flex flex-col space-y-4 border-y-2 p-2">
                         <li>
                             <a href="">
                                 <img width="24" height="24" src="https://img.icons8.com/fluency-systems-regular/48/delivery-settings.png" alt="delivery-settings"/>
@@ -19,6 +21,23 @@ export default function SideNav(){
                         <li>   
                             <a href="">
                                 <img width="24" height="24" src="https://img.icons8.com/fluency-systems-regular/48/boxes.png" alt="boxes"/>
+                            </a>
+                        </li>
+                    </div>
+                    {/* Sales Management */}
+                    <div className="flex flex-col space-y-4 border-y-2 p-2">
+                        <li>
+                            <a href="">
+                                <img width="24" height="24" src="https://img.icons8.com/fluency-systems-regular/48/total-sales.png" alt="total-sales"/>
+                            </a>
+                        </li>
+                        <li>   
+                            <a href="">
+                                <img width="24" height="24" src="https://img.icons8.com/fluency-systems-regular/48/tax.png" alt="tax"/>                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img width="24" height="24" src="https://img.icons8.com/fluency-systems-regular/48/money-box--v1.png" alt="money-box--v1"/>
                             </a>
                         </li>
                     </div>
