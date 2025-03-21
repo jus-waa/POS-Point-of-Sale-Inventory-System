67fbe13d76441b7150c4d21cc74f4df05c6d1e34
josh <joshlagrimas1808@gmail.com>
2025-03-21 22:48:55 +0800

    Added sample auth routes and controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,1 +1,13 @@
 import db from "../db/index.js";
+
+export const signupRoute = async (req, res) => {
+    res.send("Signup Route");
+};
+
+export const loginRoute = async (req, res) => {
+    res.send("Login Route");
+};
+
+export const logoutRoute = async (req, res) => {
+    res.send("Logout Route");
+};
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
 import {
-    
+    signupRoute,
+    loginRoute,
+    logoutRoute
 } from "../controllers/authController.js"
 
 const router = express.Router();
+
+router.get("/signup", signupRoute);
+router.get("/login", loginRoute);
+router.get("/logout", logoutRoute);
 
 
 export default router;
